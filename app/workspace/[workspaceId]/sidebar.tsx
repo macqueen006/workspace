21a7aa20273fbc63415624bdf924f77520fe8c8f
macqueen006 <macqueensawyer@yahoo.com>
2025-07-31 08:00:56 +0100

    added the sidebar button for navigation

--- app/workspace/[workspaceId]/sidebar.tsx
+++ app/workspace/[workspaceId]/sidebar.tsx
@@ -1,16 +1,28 @@
 "use client";
 import { UserButton } from "@/features/auth/components/user-button";
 import WorkspaceSwitcher from "./workspace-switcher";
+import { SidebarButton } from "./sidebar-button";
+import { Bell, Home, MessagesSquare, MoreHorizontal } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
-    return (
-        <aside className="w-[70px] h-full bg-slack-800 flex flex-col gap-y-4 items-center pt-[9px] pb-4">
-            <div><WorkspaceSwitcher /></div>
-            <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
-                <UserButton />
-            </div>
-        </aside>
-    );
-}
+  const pathname = usePathname();
+  return (
+    <aside className="w-[70px] h-full bg-slack-800 flex flex-col gap-y-4 items-center pt-[9px] pb-4">
+      <WorkspaceSwitcher />
+      <SidebarButton
+        icon={Home}
+        label="Home"
+        isActive={pathname.includes("/workspace")}
+      />
+      <SidebarButton icon={MessagesSquare} label="Dms" />
+      <SidebarButton icon={Bell} label="Activity" />
+      <SidebarButton icon={MoreHorizontal} label="More" />
+      <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
+        <UserButton />
+      </div>
+    </aside>
+  );
+};
 
 export default Sidebar;
