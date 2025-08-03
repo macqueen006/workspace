9464449b2bf3de51dfb096729fc0f8c3c8d430b1
macqueen006 <macqueensawyer@yahoo.com>
2025-08-03 15:06:46 +0100

    Allow user to reset invite code due to multiple joining

--- app/workspace/[workspaceId]/workspace-invite-modal.tsx
+++ app/workspace/[workspaceId]/workspace-invite-modal.tsx
@@ -4,13 +4,13 @@ import {
   DialogContent,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
   DialogDescription,
   DialogClose,
-  DialogFooter,
 } from "@/components/ui/dialog";
+import { useNewJoinCode } from "@/features/workspaces/api/use-new-join-code";
+import { useConfirm } from "@/hooks/use-confirm";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
-import { Copy } from "lucide-react";
+import { Copy, RefreshCcw } from "lucide-react";
 import { toast } from "sonner";
 
 interface InviteModalProps {
@@ -27,6 +27,28 @@ export const InviteModal = ({
   joinCode,
 }: InviteModalProps) => {
   const workspaceId = useWorkspaceId();
+  const { mutate, isPending } = useNewJoinCode();
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "This will deactivate the current invite code and generate a new one."
+  );
+
+  const handleNewCode = async () => {
+    const ok = await confirm();
+    if (!ok) return;
+
+    mutate(
+      { workspaceId },
+      {
+        onSuccess: () => {
+          toast.success("Invite code regenerated");
+        },
+        onError: () => {
+          toast.error("Failed to generate the invite code");
+        },
+      }
+    );
+  };
 
   const handleCopy = () => {
     const inviteLink = `${window.location.origin}/join/${workspaceId}`;
@@ -35,24 +57,40 @@ export const InviteModal = ({
       .then(() => toast.success("Invite link copied to clipboard"));
   };
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Invite people to {name}</DialogTitle>
-          <DialogDescription>
-            Use the code below to invite people to your workspace
-          </DialogDescription>
-        </DialogHeader>
-        <div className="flex flex-col gap-y-4 items-center justify-center py-10">
-          <p className="text-4xl font-bold tracking-widest uppercase">
-            {joinCode}
-          </p>
-          <Button onClick={handleCopy} variant="ghost" size="sm">
-            Copy link
-            <Copy className="size-4 ml-2" />
-          </Button>
-        </div>
-      </DialogContent>
-    </Dialog>
+    <>
+      <ConfirmDialog />
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Invite people to {name}</DialogTitle>
+            <DialogDescription>
+              Use the code below to invite people to your workspace
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex flex-col gap-y-4 items-center justify-center py-10">
+            <p className="text-4xl font-bold tracking-widest uppercase">
+              {joinCode}
+            </p>
+            <Button onClick={handleCopy} variant="ghost" size="sm">
+              Copy link
+              <Copy className="size-4 ml-2" />
+            </Button>
+          </div>
+          <div className="flex items-center justify-between w-full">
+            <Button
+              disabled={isPending}
+              variant="outline"
+              onClick={handleNewCode}
+            >
+              New code
+              <RefreshCcw className="size-4 m-2" />
+            </Button>
+            <DialogClose asChild>
+              <Button>Close</Button>
+            </DialogClose>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -10,6 +10,29 @@ const generateCode = () => {
   return code;
 };
 
+export const newJoinCode = mutation({
+  args: { workspaceId: v.id("workspaces") },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    // If userId is not found, throw an error
+    if (!userId) {
+      throw new Error("User not authenticated");
+    }
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId)
+      )
+      .unique();
+    if (!member || member.role !== "admin") throw new Error("Unauthorized");
+    const joinCode = generateCode();
+    await ctx.db.patch(args.workspaceId, {
+      joinCode,
+    });
+    return args.workspaceId;
+  },
+});
+
 // This file defines the API for managing workspaces in the application.
 export const create = mutation({
   args: {
